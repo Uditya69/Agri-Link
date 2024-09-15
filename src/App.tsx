819f1e8f961c2a15f374e5e6ee28f4e65d01d5d7
uditya69 <uditya04@gmail.com>
2024-09-16 01:28:54 +0530

    Scaffold vite project with TailwindCSS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className=" flex flex-col items-center justify-between m-auto ">
+      <h1 className=" font-bold">Agri-Link</h1>
+      <p>we aim to revolutionise sales for farmers!</p>
+    </div>
+  );
+}
+
+export default App;
