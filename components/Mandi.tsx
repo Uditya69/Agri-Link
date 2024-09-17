188bad560b9f49af94125e773ae1f013b2600949
smit <smitmistry36466@gmail.com>
2024-09-17 14:50:10 +0530

    added navs

---
+++ components/Mandi.tsx
@@ -0,0 +1,9 @@
+const Mandi = () => {
+  return (
+    <div>
+      <h4>mandi</h4>
+    </div>
+  );
+};
+
+export default Mandi;
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "../components/auth/Auth";
-import Home from "../components/home/index";
 import ProtectedRoute from "../components/auth/ProtectedRoute.tsx";
 import AddItem from "../components/auction/AddItem";
 import Auction from "../components/auction/Auction";
+import "./index.css";
+import ContactUs from '../components/ContactUs';
+import Home from "../components/Home";
+import Mandi from '../components/Mandi'
+import Profile from '../components/Profile'
+import BottomNav from '../components/BottomNav';
+
 const App: React.FC = () => { 
   return (
     <Router>
       <Routes>
         <Route path="/auth" element={<Auth />} />
         <Route
-          path="/home"
+          path="/"
           element={<ProtectedRoute><Home /></ProtectedRoute>}
         />
         <Route path="/add" element={<AddItem/>} />
         <Route path="/auctions"element={<Auction/>} />
+        <Route path="/contactUs" element={<ContactUs />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
+      <BottomNav/>
     </Router>
   );
 };
