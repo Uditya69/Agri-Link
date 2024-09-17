36eae1e7dada72f74963fda764441d1bf206c6db
Outrages7 <rana.772003shubham@gmail.com>
2024-09-17 14:31:11 +0530

    Loader

--- components/auth/Auth.tsx
+++ components/auth/Auth.tsx
@@ -6,7 +6,7 @@ const Auth: React.FC = () => {
   const [isLogin, setIsLogin] = useState(true);
 
   return (
-    <div className="flex flex-col items-center justify-center w-full m-auto  h-screen bg-gray-200 p-4">
+    <div className="flex flex-col items-center justify-center w-full m-auto  h-screen  p-4">
       <div className="flex flex-col overflow-scroll  min-h-[55%] min-w-[40%]  items-center justify-between m-auto p-4 bg-white rounded-lg shadow-md">
         <div className="flex justify-between gap-5 mb-6 border-b border-gray-300">
           <button
